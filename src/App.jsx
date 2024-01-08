10ad914483da2e14d6c16cb7049fd9465c304b9f
RANJAN <141898565+mranja@users.noreply.github.com>
2024-01-08 10:19:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import HigherOrderFunctions from "./components/HigherOrderFunctions";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HigherOrderFunctions />
+      </div>
+    </>
+  );
+}
+
+export default App;
